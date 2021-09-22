0f695c018aa80e5dc141c834be311f4ea18359b2
Artem Halishchuk <artem.galishchuk@gmail.com>
2021-09-22 19:06:15 +0300

    -

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    class slider {
+    class Slider {
+        static FRAME_TIME = 50;
+        static SLIDE_TIME = 5000;
         constructor(selector) {
             this.selector = selector;
+            this.FRAME
         }
         init() {
             this.slider = document.querySelector(this.selector);
@@ -11,11 +14,12 @@ document.addEventListener('DOMContentLoaded', function() {
             this.bindEvents();
         }
         bindEvents() {
-            this.arrowrRight.addEventListener('click', ()=>this.nextSlide());
+            this.arrowrRight.addEventListener('click', ()=>this.nextSlide(Slider.SLIDE_TIME));
         }
 
-        nextSlide() {
-
+        nextSlide(time) {
+            let frameCount = time/Slider.FRAME_TIME;
+            let step = 50/frameCount
         }
     }
 })
