514ee706510a6b29a3e83e8f8d53e744e2645c85
Artem Halishchuk <artem.galishchuk@gmail.com>
2021-09-22 19:00:37 +0300

    -

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    alert('ok');
+    class slider {
+        constructor(selector) {
+            this.selector = selector;
+        }
+        init() {
+            this.slider = document.querySelector(this.selector);
+            this.wrapper = this.slider.querySelector('.slider__wrapper');
+            this.arrowLeft = this.slider.querySelector('.slider__arrow-left');
+            this.arrowrRight = this.slider.querySelector('.slider__arrow-right');
+            this.bindEvents();
+        }
+        bindEvents() {
+            this.arrowrRight.addEventListener('click', ()=>this.nextSlide());
+        }
+
+        nextSlide() {
+
+        }
+    }
 })
