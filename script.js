3936c114b5a16663640364e952a7f914de530b31
Artem Halishchuk <artem.galishchuk@gmail.com>
2021-09-22 19:36:39 +0300

    -

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ class Slider {
     }
 
     nextSlide(time) { //быстрая прокрутка, мотать к 3-5 слайду, передавать меньшее время
+        if (this.timer !== null) return;
         let frameCount = time/Slider.FRAME_TIME;
         let step = 100/frameCount;
         let currentPosition = 0;
@@ -22,6 +23,8 @@ class Slider {
             if(currentPosition <= -100) {
                 clearInterval(this.timer);
                 this.timer = null; //флаг идет ли анимация
+
+
                 this.wrapper.append(this.wrapper.children[0]); //после остановки перекидываем слайды
                 this.wrapper.style.marginLeft = '';
                 //stop
@@ -32,9 +35,15 @@ class Slider {
         }, Slider.FRAME_TIME);
     }
 }
-Slider.FRAME_TIME = 50;
+Slider.FRAME_TIME = 16;
 Slider.SLIDE_TIME = 2000;
 document.addEventListener('DOMContentLoaded', function() {
     let slider = new Slider('.slider');
     slider.init();
 })
+
+//в другую сторону обратный порядок
+//1. действия по маргину, смещаем туда
+//2. потом крутим сюда
+//3. перед таймером переместим на тот слайд и добавим маргинов
+//взять последний элемент сделать prepend во врапер и крутим в обратную сторону
